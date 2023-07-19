36e44572e95220982466c94a847743c840391fc4
paquinho89 <paquinho89@hotmail.com>
2023-07-19 18:13:11 +0200

    country_information

--- bicicleteiros/models.py
+++ bicicleteiros/models.py
@@ -8,6 +8,11 @@ with open("bicicleteiros/static/lists/country_list.txt", "r") as country_list_fi
     #A regular expresion é para eliminar o salto de línea que contén toda a lista e que me funcione despois no formulario. Co [0] estou collendo o primer resultado da función re.search
     country_list_fixed = tuple(([re.search('[^\n]*',country)[0],re.search('[^\n]*',country)[0]]) for country in country_list)
 
+with open("bicicleteiros/static/lists/time_zone_list.txt", "r") as time_zone_list_file:
+    time_zone_list = tuple(time_zone_list_file)
+    #A regular expresion é para eliminar o salto de línea que contén toda a lista e que me funcione despois no formulario. Co [0] estou collendo o primer resultado da función re.search
+    time_zone_list_fixed = tuple(([re.search('[^\n]*',time_zone)[0],re.search('[^\n]*',time_zone)[0]]) for time_zone in time_zone_list)
+
 # Create your models here.
 day_type_choices = (
     ('resting','resting'),
@@ -19,10 +24,11 @@ night_type_choices = (
     ('accommodation', 'accommodation')
 )
 
-#This is to prepopulate the day of the journey
-today_date = datetime.now().date()
-#IMPORTANTE: Eiqui tes que cambiar a data e por a data na que comezas a viaxe.
+#THIS IS TO POPULATE THE DAY OF THE JOURNEY
+#IMPORTANTE!!!: Eiqui tes que cambiar a data e por a data na que comezas a viaxe.
 starting_date = datetime(2023, 7, 1).date()
+
+today_date = datetime.now().date()
 day_in_the_journey = today_date - starting_date
 day_in_the_journey_final = str(day_in_the_journey).split(" ",1)[0]
 
@@ -38,6 +44,7 @@ class summary_day_model(models.Model):
     money_transport = models.DecimalField(max_digits=6, decimal_places=2, blank = True, null=True)
     money_burocracy = models.DecimalField(max_digits=6, decimal_places=2, blank = True, null=True)
     money_others = models.DecimalField(max_digits=6, decimal_places=2, blank = True, null=True)
+    #currency cambiado a Euros
     km_first_stop = models.IntegerField(blank = True, null=True)
     km_second_stop = models.IntegerField(blank = True, null=True)
     km_third_stop = models.IntegerField(blank = True, null=True)
@@ -47,32 +54,29 @@ class summary_day_model(models.Model):
     altitude_third_stop = models.IntegerField(blank = True, null=True)
     altitude_fourth_stop = models.IntegerField(blank = True, null=True)
 
-    # @property
-    # def journey_day (self):
-    #     today = datetime.now().date()
-    #     day_in_journey = today - self.starting_date
-    #     day_in_journey_just_days = str(day_in_journey).split(",",1)[0]
-    #     return day_in_journey_just_days
+    def __str__(self):
+        return (str(self.journey_day_model) + ' day' )
 
 
 class country_information_model(models.Model):
     country = models.CharField(max_length=33, choices= country_list_fixed, blank=True, null=True)
-    population = models.IntegerField(blank=True, null=True)
-    rent_per_capita = models.IntegerField(blank=True, null=True)
+    capital_town = models.CharField(max_length=33, blank=True, null=True)
     surface = models.IntegerField(blank=True, null=True)
-    #surface_comparison with Spain
-    regime = models.CharField(max_length=33, choices= country_list_fixed, blank=True, null=True)
+    population = models.IntegerField(blank=True, null=True)
+    population_density = models.IntegerField(blank=True, null=True)
+    rent_per_capita = models.IntegerField(blank=True, null=True)
     currency = models.CharField(max_length=33, choices= country_list_fixed, blank=True, null=True)
-    #currency cambiado a Euros
-    local_time = models.TimeField(blank = True, null = True)
+    time_zone = models.CharField(max_length=33, choices= time_zone_list_fixed, blank=True, null=True)
+    visa_requerided = models.CharField(max_length=33, choices= (('yes', 'yes'), ('no','no')), blank=True, null=True)
+    visa_price = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return (self.country)  
     
     #Calculate the surface_comparison_field (The comparison is done with Spain):
-    @property
-    def surface_comparison (self):
-        return 41
+    # @property
+    # def surface_comparison (self):
+    #     return 41
 
 
 
--- bicicleteiros/views.py
+++ bicicleteiros/views.py
@@ -1,20 +1,55 @@
 from django.shortcuts import render
-from .models import country_information_model
+from .models import country_information_model, summary_day_model, starting_date
 from datetime import datetime, date
+#Packages to create the local clock
+# from tkinter  import *
+# from tkinter.ttk import *
+# from time import strftime
+
+# root = Tk()
+# root.title ("clock")
+
+# label = Label(root, font=("ds-digital", 80), background="black", foreground="cyan")
+# label.pack(anchor='center')
+
+# def local_clock ():
+#     string = strftime('%H:%M:%S')
+#     label.config(text=string)
+#     label.after(1000, local_clock)
+
+# label = Label(root, font=("ds-digital", 80), background="black", foreground="cyan")
+# label.pack(anchor='center')
+
+# local_clock()
+# mainloop()
 
 # Create your views here.
 
 def country_data_view (request):
-    #country = country_information_model.objects.get (country ="France" )
     today = datetime.now().date()
-    #TES QUE POR EIQUI O DÍA QUE COMEZAS A TÚA VIAXE.
-    starting_date = datetime(2023, 7, 2).date()
     day_in_journey = today - starting_date
-    print(type(day_in_journey))
     day_in_journey_just_days = str(day_in_journey).split(",",1)[0]
+    current_country = summary_day_model.objects.get(journey_day_model = day_in_journey_just_days.split(" ",1)[0] ).country
+    capital_city = country_information_model.objects.get(country = current_country).capital_town
+    population_country = country_information_model.objects.get(country = current_country).population
+    population_dens = country_information_model.objects.get(country = current_country).population_density
+    rent_per_capita_country = country_information_model.objects.get(country = current_country).rent_per_capita
+    surface_country = country_information_model.objects.get(country = current_country).surface
+    currency_country = country_information_model.objects.get(country = current_country).currency
+    currency_country = country_information_model.objects.get(country = current_country).currency
+    time_zone_value = country_information_model.objects.get(country = current_country).time_zone
+    print(time_zone_value)
+    
     context = {
-        #'country_html' : country,
-        'journey_day_html' : day_in_journey_just_days
+        'journey_day_html' : day_in_journey_just_days,
+        'current_country_html' : current_country,
+        'capital_city_html' : capital_city,
+        'surface_html' : surface_country,
+        'population_html' : population_country,
+        'density_population_html' : population_dens,
+        'rent_per_capita_html' : rent_per_capita_country,
+        'currency_html' : currency_country,
+        'time_zone_html' : time_zone_value
     }
     return render (request, 'bicicleteiros_home_page.html', context)
 
