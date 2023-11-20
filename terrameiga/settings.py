4a411aecb62f942d5b8208d66b2405ebb607b31e
paquinho89 <paquinho89@hotmail.com>
2023-11-20 15:30:40 +0100

    django_deployment

--- terrameiga/settings.py
+++ terrameiga/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'bicicleteiros',
     #Esto é para o rich text
     'ckeditor',
+    'storages',
 
 ]
 # This is to indicate to Django that the user model is now this custom model (CustomeUser) instead of the User model
@@ -204,6 +205,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 print('MEDIA URLS')
 print(MEDIA_ROOT)
 print(MEDIA_URL)
+
+
+
+AWS_ACCESS_KEY_ID = 'AKIAYMHV2VTD3ADZHAID'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'terrameiga'
+
+AWS_S3_FILE_OVERWRITE = True #Quero que cando se suba un arquivo co mesmo nome este se reemplace"
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
