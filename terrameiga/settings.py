a21999bdf6d6d3293865884d70337599a76dc046
paquinho89 <paquinho89@hotmail.com>
2023-11-20 15:39:07 +0100

    django_deployment

--- terrameiga/settings.py
+++ terrameiga/settings.py
@@ -216,6 +216,7 @@ AWS_S3_FILE_OVERWRITE = True #Quero que cando se suba un arquivo co mesmo nome e
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE =  'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
