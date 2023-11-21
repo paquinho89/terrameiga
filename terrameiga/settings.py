3c40b1ad9554dbb6d71d9736a52d7e80b4bfe4f5
paquinho89 <paquinho89@hotmail.com>
2023-11-21 16:17:21 +0100

    django_deployment

--- terrameiga/settings.py
+++ terrameiga/settings.py
@@ -204,10 +204,8 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 #MEDIA FILES: Estes son arquivos que subo ou suben a app os usuarios e que se van a gardar no bucket de terrameiga en S3 que se chama: "media_files".
 #FILES STORAGE configuración en AWS (S3)
-MEDIA_URL = 'https://terrameiga/'
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-#AWS_MEDIA = 'mediafiles'
 AWS_STORAGE_BUCKET_NAME = 'terrameiga'
 AWS_S3_FILE_OVERWRITE = True #Quero que cando se suba un arquivo co mesmo nome este se reemplace"
 AWS_DEFAULT_ACL = None
