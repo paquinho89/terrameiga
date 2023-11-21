b9478ccdae8cb6d14fbffe90155d4857e34deb18
paquinho89 <paquinho89@hotmail.com>
2023-11-21 16:26:42 +0100

    django_deployment

--- terrameiga/settings.py
+++ terrameiga/settings.py
@@ -195,13 +195,6 @@ STATICFILES_DIRS=[
 #Esto é para asignarlle un sitio a carpeta que se crea cando se fai o "python manage.py collectstatic"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#STATIC FILES - Telo que facer así tal cual, porque senon non che vai a funcionar
-# AWS_S3_CUSTOM_DOMAIN = 'terrameiga.s3.eu-west-3.amazonaws.com'  # Specify your custom domain here
-# AWS_LOCATION = 'staticfiles'
-# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
 #MEDIA FILES: Estes son arquivos que subo ou suben a app os usuarios e que se van a gardar no bucket de terrameiga en S3 que se chama: "media_files".
 #FILES STORAGE configuración en AWS (S3)
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
@@ -210,6 +203,12 @@ AWS_STORAGE_BUCKET_NAME = 'terrameiga'
 AWS_S3_FILE_OVERWRITE = True #Quero que cando se suba un arquivo co mesmo nome este se reemplace"
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+#STATIC FILES - Telo que facer así tal cual, porque senon non che vai a funcionar
+AWS_S3_CUSTOM_DOMAIN = 'terrameiga.s3.eu-west-3.amazonaws.com'  # Specify your custom domain here
+AWS_LOCATION = 'staticfiles'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Default primary key field type
