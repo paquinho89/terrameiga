e716a8187e0b8b4d3a7b26a6b79bd2452ed78937
paquinho89 <paquinho89@hotmail.com>
2023-11-21 16:56:15 +0100

    media_files_fixing

--- terrameiga/settings.py
+++ terrameiga/settings.py
@@ -204,8 +204,8 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #MEDIA FILES: Estes son arquivos que subo ou suben a app os usuarios e que se van a gardar no bucket de terrameiga en S3 que se chama: "media_files".
 #FILES STORAGE configuración en AWS (S3)
 #MEDIA FILES: Estes son arquivos que subo ou suben a app os usuarios e que se van a gardar no bucket de terrameiga en S3 que se chama: "media_files".
-MEDIA_URL = '/media_files/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'terrameiga'
