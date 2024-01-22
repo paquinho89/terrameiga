39a5bf1f34ef6b229a193a241faf944961d02a0a
paquinho89 <paquinho89@hotmail.com>
2024-01-22 18:21:58 +0100

    handling_error

--- terrameiga/settings.py
+++ terrameiga/settings.py
@@ -60,10 +60,6 @@ INSTALLED_APPS = [
     'storages',
     #ESto é para que aparezan os puntos nos datos dos países e os números sexan máis visibles
     'django.contrib.humanize',
-    #Esto é un archivo que está en terrameiga.middleware.py e que é o encargado de manexar errores que os customicei eu. Básicamente, cando hai un error redirixo a xente a 
-    #un html preparado por min (error.html)
-    'terrameiga.middleware.ErrorRedirectMiddleware',
-
 ]
 # This is to indicate to Django that the user model is now this custom model (CustomeUser) instead of the User model
 #IMPORTANTE! No caso de que "python manage.py migrate" non che funcione, referencialo a app que sexa.
@@ -86,6 +82,9 @@ MIDDLEWARE = [
     # Failing that, it looks at the Accept-Language HTTP header. This header is sent by your browser and tells the server which language(s) you prefer, in order of priority. Django tries each language in the header until it finds one with available translations.
     # Failing that, it uses the global LANGUAGE_CODE setting.
     'django.middleware.locale.LocaleMiddleware',
+    #Esto é un archivo que está en terrameiga.middleware.py e que é o encargado de manexar errores que os customicei eu. Básicamente, cando hai un error redirixo a xente a 
+    #un html preparado por min (error.html)
+    'terrameiga.middleware.ErrorRedirectMiddleware',
 ]
 
 ROOT_URLCONF = 'terrameiga.urls'
