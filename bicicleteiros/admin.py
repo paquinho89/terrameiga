7e0c55d97c856a4ae3cffb00aee5651748e5b96f
paquinho89 <paquinho89@hotmail.com>
2024-02-15 21:11:49 +0100

    numero_pais

--- bicicleteiros/admin.py
+++ bicicleteiros/admin.py
@@ -5,10 +5,15 @@ from bicicleteiros.models import currency, country_information_model, money_mode
 # Register your models here.
 
 admin.site.register(currency)
-admin.site.register(country_information_model)
+#admin.site.register(country_information_model)
+@admin.register(country_information_model)
+class CountryInformationAdmin(admin.ModelAdmin):
+    list_display = ['country', 'country_number']
 admin.site.register(money_model)
 admin.site.register(km_altitude_model)
 admin.site.register(chat_comments_model)
 admin.site.register(chat_comments_replies_model)
 admin.site.register(videos_model)
 admin.site.register(photos_model)
+
+
